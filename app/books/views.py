c88cbf9a45ea60e7813be572519c3c6f66e9a0f1
Riku <rikukestila@gmail.com>
2022-01-25 17:04:04 +0200

    Add Django unicorn

--- app/books/views.py
+++ app/books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Book
 
 # Create your views here.
+def index(request):
+    books = Book.objects.all()
+    return render(request, "books/index.html", {
+        "books": books 
+    })
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "books/book_detail.html", {
+            "title": book.title,
+            "author": book.author,
+            "rating": book.rating,
+            "os_bestselling": book.is_bestselling
+    })
+    
