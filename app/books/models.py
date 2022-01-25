81807e0e81012245cc81c0ae8b98bfd49efbd7e4
Riku <rikukestila@gmail.com>
2022-01-25 15:06:22 +0200

    Add Book model

--- app/books/models.py
+++ app/books/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.auth.models import AbstractUser
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class CustomUser(AbstractUser):
     pass
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
