db2d5b4045ad5f6b9b37c7c5c28f71fa42553975
Riku <rikukestila@gmail.com>
2022-02-16 11:55:07 +0200

    Create views for Book create via HTMX

--- app/books/urls.py
+++ app/books/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="starting-page"),
-    path("<slug:slug>", views.book_detail, name="book-detail")
+    path("book/<slug:slug>/", views.book_detail, name="book-detail"),
+    path("author/<pk>/", views.create_book, name="create-book"),
+    path("htmx/create-book-form/", views.create_book_form, name="create-book-form"),
+    path("htmx/book/<pk>/", views.detail_book, name="detail-book")
 ]
--- app/books/views.py
+++ app/books/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import get_object_or_404, render
+from webbrowser import get
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Avg
-from books.models import Book
+from .forms import BookForm
+from .models import Book, Author
+
 
 # Create your views here.
 def index(request):
+    """List all books"""
     books = Book.objects.all().order_by("title")
     num_books = books.count()
     avg_rating = books.aggregate(Avg("rating"))
@@ -15,6 +20,7 @@ def index(request):
     })
 
 def book_detail(request, slug):
+    """Get a single book"""
     book = get_object_or_404(Book, slug=slug)
     return render(request, "books/book_detail.html", {
             "title": book.title,
@@ -22,3 +28,48 @@ def book_detail(request, slug):
             "rating": book.rating,
             "os_bestselling": book.is_bestselling
     })
+
+
+def create_book(request, pk):
+    """Create a book"""
+    author = Author.objects.get(id=pk)
+    books = Book.objects.filter(author=author)
+    form = BookForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.author = author
+            book.save()
+            return redirect("detail-book", pk=book.id)
+        else:
+            return render(request, "books/partials/book_form.html", context={
+                "form": form
+            })
+
+
+    context = {
+        "form": form,
+        "author": author,
+        "books": books,
+        "title": "Create a book"
+    }
+
+    return render(request, "books/create_book.html", context)
+
+def create_book_form(request):
+    """Return the form for HTMX create form"""
+    form = BookForm()
+    context = {
+        "form": form
+    }
+
+    return render(request,"books/partials/book_form.html", context)
+
+def detail_book(request,pk):
+    book = get_object_or_404(Book, id=pk)
+    context = {
+        "book": book
+    }
+
+    return render(request, "books/partials/book_detail.html", context)
